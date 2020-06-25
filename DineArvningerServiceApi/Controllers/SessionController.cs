cbe22d60b9575cf75065092f3cdec317d28fd700
MoActive <arief@live.dk>
2020-06-25 11:41:06 +0200

    Added CORS

--- DineArvningerServiceApi/Controllers/SessionController.cs
+++ DineArvningerServiceApi/Controllers/SessionController.cs
@@ -6,12 +6,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace DineArvningerServiceApi.Controllers
 {
+    [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
     public class SessionController : ApiController
     {
-
         [HttpPost]
         public string createSession(Session session)
         {
--- DineArvningerServiceApi/Controllers/TemplateFormController.cs
+++ DineArvningerServiceApi/Controllers/TemplateFormController.cs
@@ -6,9 +6,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace DineArvningerServiceApi.Controllers
 {
+    [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
     public class TemplateFormController : ApiController
     {
 
